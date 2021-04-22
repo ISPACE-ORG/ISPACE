e3959dc302dc2b563b23d2eabc712e24149a93e4
ISPACE-ORG <83037083+ISPACE-ORG@users.noreply.github.com>
2021-04-23 02:40:30 +0430

    Create ispace.php

---
+++ ispace.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>7Learn</title>
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            background: #007bec;
+            color: #fff;
+            font-size: 1.5em;
+            font-weight: 400;
+            text-align: right;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        .main {
+            position: relative;
+            background: #ffffff;
+            color: #0e5698;
+            display: inline-block;
+            padding: 50px;
+            margin: 0 auto;
+            border-radius: 7px;
+            box-shadow: 0 50px 50px rgba(0, 0, 0, 0.2);
+            min-height: 250px;
+            min-width: 400px;
+        }
+
+        h1 {
+            font: 30px sans-serif;
+            text-align: center;
+            margin: 25px 10px;
+            font-weight: bold;
+            color: #4a96db;
+        }
+
+        input,
+        button,
+        select,
+        textarea {
+            display: block;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+            margin-top: 10px;
+            line-height: 28px;
+            height: 30px;
+            padding:10px;
+            width: 100%;
+            box-sizing: border-box;
+            font-family: sans-serif;
+            font-size: 15px;
+        }
+
+        button {
+            background: #007bec;
+            color: #fff;
+            line-height: 0px;
+            height: 40px;
+            font-size: 18px;
+            border: 0;
+            padding:20px;
+            margin-bottom: 10px;
+            min-height: 20px;
+            max-height: 120px;
+        }
+
+        button:hover {
+            opacity: 0.7;
+            cursor: pointer;
+        }
+        p {
+            font: 17px sans-serif;
+            text-align: center;
+            margin: 25px 10px;
+            color: #000;
+        }
+        #login{
+            height: 20px;
+            padding-top: 20px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class='main'>
+        <h1>I SPACE</h1>
+        <br>
+        <button type="submit"  name="sign_up" onclick="location.href='index.php';">sign_up</button>
+            <br>
+            <button type="submit"   name="login"onclick="location.href='login.php';" >Login</button>
+            <br>
+            <p>producted by:Ali Reza Jafari</p>
+        </form>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
